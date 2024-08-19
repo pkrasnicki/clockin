b8db66392f185383de57a773c8f596157de4bfff
Paweł Kraśnicki <krasnicki.pawel@gmail.com>
2024-08-19 15:06:18 +0200

    version update

--- src/Console/Application.php
+++ src/Console/Application.php
@@ -14,7 +14,7 @@ final class Application extends SymfonyApplication
 {
     public function __construct()
     {
-        parent::__construct('ClockIn', '0.0.4');
+        parent::__construct('ClockIn', '0.0.5');
     }
 
     protected function getDefaultInputDefinition(): InputDefinition
